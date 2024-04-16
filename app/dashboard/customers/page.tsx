2965660667374db5edbfd7a0b700b067b8ffce10
roberto <robertohoungbo@gmail.com>
2024-04-16 16:58:22 +0100

    Description de vos modifications

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CustomerPage() {
+  return (
+    <div>
+      Customers
+    </div>
+  )
+}
+
+export default CustomerPage
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InvoicesPage() {
+  return (
+    <div>
+      Invoices
+    </div>
+  )
+}
+
+export default InvoicesPage
